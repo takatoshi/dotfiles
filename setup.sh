eadc7e3ea92199a788be33af307e9c34b597f0dd
Takatoshi <kobayashi.takatoshi@gmail.com>
2013-06-14 01:21:44 +0900

    vim-powerlineをsubmodule、少し修正

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ git config --global color.ui "auto"
 #NeoBundleのサブモジュール
 git submodule init -- vim/bundle/neobundle.vim
 git submodule update -- vim/bundle/neobundle.vim
+#tmux-powerlineのサブモジュール
+git submodule init -- tmux-powerline
+git submodule update -- tmux-powerline
