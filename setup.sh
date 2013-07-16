bc957939295b1db11b48bb6adbb480ee7ec4d896
kobayashi <kobayashi@devexblog-web10>
2013-07-16 18:27:52 +0900

    setupのgit設定、vim/vim削除。zshrcのPATH変更

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 #シンボリックリンク作成
 ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
@@ -7,8 +9,15 @@ ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
-#gitのカラー自動設定
+# なぜか生成されてしまうファイル削除
+rm -f ~/dotfiles/vim/vim
+
+#git設定
 git config --global color.ui "auto"
+git config --global core.pager ''
+git config --global user.email $USER@$HOST
+git config --global user.name $USER
+
 
 #NeoBundleのサブモジュール
 git submodule init -- vim/bundle/neobundle.vim
@@ -16,3 +25,4 @@ git submodule update -- vim/bundle/neobundle.vim
 #tmux-powerlineのサブモジュール
 git submodule init -- tmux-powerline
 git submodule update -- tmux-powerline
+
--- zshrc
+++ zshrc
@@ -90,4 +90,6 @@ export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46
 alias gls="gls --color"
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
 # PATHに追加
-export PATH=~/local/bin:~/pear:$PATH
+export PATH=~/local/bin:~/libs/pear:$PATH
+# HOSTをシェル内でも使えるように
+export HOST
